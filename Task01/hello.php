523ddec150f98978e1801dd0e4c77941064b229b
darkflade <dodgesrt7775@gmail.com>
2025-02-06 20:51:43 +0300

    Just added

---
+++ Task01/hello.php
@@ -0,0 +1,10 @@
+<?php
+
+$currTimeDate = date('d-m-Y h:i:s');
+$name = "Новиков Максим Евгеньевич";
+
+echo "$name \n";
+echo "----------------------------- \n";
+echo "Сейчас $currTimeDate";
+
+?>
