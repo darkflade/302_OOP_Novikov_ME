046f36ba65707a0266bad369a82ef034ba6ca43f
Pazhiloy Zhmex <dodgesrt7775@gmail.com>
2025-02-06 21:05:15 +0300

    Add Timezone.php

--- Task01/hello.php
+++ Task01/hello.php
@@ -1,10 +1,11 @@
 <?php
 
-$currTimeDate = date('d-m-Y h:i:s');
+$timezone = new DateTimeZone('Europe/Moscow');
+$currTimeDate = new DateTime('now', $timezone)->format('d-m-Y H:i:s');
 $name = "Новиков Максим Евгеньевич";
 
 echo "$name \n";
-echo "----------------------------- \n";
+echo "--------------------------\n";
 echo "Сейчас $currTimeDate";
 
 ?>
